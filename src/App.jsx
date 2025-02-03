30e33ca88f4d25cb9783f86f736278dc1b6ca461
Sofiamitrathakur <Smitrathakur@bloomberg.net>
2025-02-03 15:04:21 +0800

    adds components, first draft of jsx files, adds styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
+import albums from 'albums.json'
+import Album from './components/Album.jsx'
 
 const App = () => {
-  return <main></main>
+  console.log(albums)
+  return (
+    <div className="App">
+      <h1>Favourite Albums</h1>
+      {albums.results.map((album) => (
+        <Album
+          cover={album.albumCover}
+          artist={album.artist.name}
+          releaseYear={album.releaseYear}
+          label={album.label}
+          favouriteSong={album.favouriteSong}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App
